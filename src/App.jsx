0b87b4e5a75e8ef356433e7e20f72b9f8f7aae2c
Stefan Hauschildt <stefan.h@uschildt.de>
2018-12-12 23:01:33 +0100

    - make news details public

--- src/App.jsx
+++ src/App.jsx
@@ -135,7 +135,7 @@ class App extends Component {
               {/* modals */}
               <Route path='**/m/' component={AccountPage}/>
               <PrivateRoute path='**/course/:id' component={CourseDetails}/>
-              <PrivateRoute path='**/news/:id' component={NewsDetails}/>
+              <Route path='**/news/:id' component={NewsDetails}/>
               <PrivateRoute path='**/membership/:id' component={MembershipDetails}/>
 
               {/* redirect everything to home */}
